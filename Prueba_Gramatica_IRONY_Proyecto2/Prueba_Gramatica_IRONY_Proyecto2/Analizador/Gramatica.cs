fad7be09edc1e4923fcd2df1bddcb7789140a8d9
ricardcutzh <ricardcutzh@gmail.com>
2017-04-06 22:30:53 -0600

    GRAMATICA CON FUNCIONES, PROCEDIMIENTOS, VARIABLES GLOBALES
    GRAMATICA CON FUNCIONES, PROCEDIMIENTOS, VARIABLES GLOBALES

--- Prueba_Gramatica_IRONY_Proyecto2/Prueba_Gramatica_IRONY_Proyecto2/Analizador/Gramatica.cs
+++ Prueba_Gramatica_IRONY_Proyecto2/Prueba_Gramatica_IRONY_Proyecto2/Analizador/Gramatica.cs
@@ -59,7 +59,7 @@ namespace Prueba_Gramatica_IRONY_Proyecto2.Analizador
             var not = ToTerm("!");
             //OPERADORES
             var mas = ToTerm("+");
-            var menor = ToTerm("-");
+            var menos = ToTerm("-");
             var por = ToTerm("*");
             var div = ToTerm("/");
             var pot = ToTerm("^");
@@ -112,13 +112,17 @@ namespace Prueba_Gramatica_IRONY_Proyecto2.Analizador
             E = new NonTerminal("E"),
             T = new NonTerminal("T"),
             G = new NonTerminal("G"),
-            F = new NonTerminal("F");
+            F = new NonTerminal("F"),
+            PROCEDIMIENTOS = new NonTerminal("PROCEDIMIENTOS"),
+            PARAMETROS = new NonTerminal("PARAMETROS"),
+            FUNCIONES = new NonTerminal("FUNCIONES"),
+            RETORNAR = new NonTerminal("RETORNAR");
             #endregion
 
             #region Gramatica
             //------------------GRAMATICA DEL CUERPO GENERAL DEL LIENZO--------------------------
 
-            INICIO.Rule = VISIBILIDAD + Lienzo + identificador + EXTIENDE + opk + CUERPOLIENZO +  clk;
+            INICIO.Rule = VISIBILIDAD + Lienzo + identificador + EXTIENDE + opk + CUERPOLIENZO + clk;
             VISIBILIDAD.Rule = publico
                             | privado
                             | Empty;
@@ -129,10 +133,12 @@ namespace Prueba_Gramatica_IRONY_Proyecto2.Analizador
 
             //------------------CUERPO DEL LIENZO ------------------------------------------------
 
-            CUERPOLIENZO.Rule = PRINCIPAL + OTROS;
+            CUERPOLIENZO.Rule = PRINCIPAL + OTROS;//RECORDAR PONER VARIABLES GLOBALES ANTES DEL METODO PRINCIPAL
             PRINCIPAL.Rule = principal + opp + clp + opk + clk
                             | Empty;
-            OTROS.Rule = VARGLOBALES + OTROS;
+            OTROS.Rule = VARGLOBALES + OTROS
+                         | PROCEDIMIENTOS + OTROS
+                         | Empty;
 
             //------------------DECLARACION DE VARIABLES GLOBALES --------------------------------
             VARGLOBALES.Rule = CONSERVAR + vr + TIPO + TIPOASIGNACION + finSent;
@@ -155,10 +161,51 @@ namespace Prueba_Gramatica_IRONY_Proyecto2.Analizador
             DIMENSIONES.Rule = opb + EXPR + clb + DIMENSIONES
                        | opb + EXPR + clb;
 
-            LLENADOARR.Rule = LISTADOARR + com + LISTADOARR
+            LLENADOARR.Rule = opl + LISTADOARR + cll
+                       | Empty;
+
+            LISTADOARR.Rule = LISTADOARR + com + LISTADOARR
                        | opl + LISTADOARR + cll
                        | EXPR;
 
+            //EXPRESIONES PARA OPERACIONES
+            EXPR.Rule = E;
+
+            E.Rule = E + mas + T
+                   | E + menos + T
+                   | T;
+
+            T.Rule = T + por + G
+                   | T + div + G
+                   | G;
+
+            G.Rule = G + pot + F
+                   | F;
+
+            F.Rule = opp + E + clp
+                   | identificador
+                   | entero
+                   | identificador + DIMENSIONES
+                   | dec
+                   | verd
+                   | fals
+                   | caracter
+                   | cadena;
+
+            //GRAMATICA DE PROCEDIMIENTOS
+
+            PROCEDIMIENTOS.Rule = CONSERVAR + identificador + opp + PARAMETROS + clp + opk + clk;
+
+            PARAMETROS.Rule = PARAMETROS + com + PARAMETROS
+                            | TIPO + identificador
+                            | Empty;
+
+            // GRAMATICA DE FUNCIONES
+
+            FUNCIONES.Rule = CONSERVAR + TIPO + identificador + opp + PARAMETROS + clp + opk + RETORNAR + finSent + clk;
+
+            RETORNAR.Rule = identificador
+                            | EXPR;
             #endregion
 
             #region Preferencias
